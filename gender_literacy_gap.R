0976c69f245dcf723462ece84ec7fd4e72a64790
Rishabh Shah <rishabhshah@MacBook-Air.local>
2024-11-21 10:47:48 +0530

    modifying nationwide calculation

--- gender_literacy_gap.R
+++ gender_literacy_gap.R
@@ -22,7 +22,14 @@ gl_df_to_map <- states %>%
   merge(gl_df, on="area_name") %>%
   mutate(border_show=area_name %in% c('ANDAMAN & NICOBAR','LAKSHADWEEP'))
 
-NATIONWIDE_AVG <- mean(gl_df_to_map$gender_gap)
+NATIONWIDE_AVG <- read_excel("data_dump/india/DDW-0000C-09.xlsx",sheet=2) %>%
+  filter(urbanity=='Total' & area_name=='INDIA' & age_group=='Total') %>%
+  filter(religion == 'All religious communities') %>%
+  mutate(female_literacy = literate_females/tot_females
+         , male_literacy = literate_males/tot_males
+         , gender_gap=(male_literacy-female_literacy)*1e2) %>%
+  pull(gender_gap)
+
 
 gl_map <- gl_df_to_map %>%
   ggplot(aes(fill=gender_gap,linewidth=border_show,color=gender_gap))+
@@ -31,13 +38,13 @@ gl_map <- gl_df_to_map %>%
   scale_linewidth_manual(values=c(0,0.5))+
   scale_color_gradient2(low="#1B9E77",mid="white",high="#d7191c",midpoint=NATIONWIDE_AVG)+
   theme_void()+
-  labs(title="India's Gender Gap in Literacy"
+  labs(title="India's Gender Gap in Literacy (2011)"
        , subtitle="calculated as: % male literacy minus (-) % female literacy"
        , caption="Prepared by Dr. Rishabh Shah (linkedin.com/in/rishabh-shah-phd-36196246/)\nData Source: Indian Census Data 2011, accessed at censusindia.gov.in"
        , fill="Gender gap (% points)")+
   theme(legend.position = "inside"
         , legend.direction = "horizontal"
-        , legend.position.inside=c(0.83,0.38)
+        , legend.position.inside=c(0.8,0.38)
         , plot.title = element_text(face = "bold", hjust=0.5, color="white",size=15)
         , plot.subtitle = element_text(hjust=0.5, color="white",size=13)
         , plot.caption = element_text(hjust=0.95, color="white")
@@ -48,7 +55,7 @@ gl_map <- gl_df_to_map %>%
         , legend.key.height = unit(0.7, "cm"))+
   annotate("segment", x = 93, y = 20, xend = 93, yend = 19,
            arrow = arrow(type = "closed"),col="white",linewidth=0.0001)+ 
-  annotate("text",label=sprintf("Nationwide average: %0.1f%%",NATIONWIDE_AVG)
+  annotate("text",label=sprintf("Nationwide gap: %0.1f%%",NATIONWIDE_AVG)
           , x = 93, y=20.3,
           col="white", size=5, vjust=0)+
   guides(linewidth="none",color="none")+
